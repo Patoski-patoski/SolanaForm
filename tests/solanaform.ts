5fd147183577aa7f0a8939ff09ea80612ac2299c
Okafor Patrick Chibuike <Patrickpeters911@gmail.com>
2025-11-09 06:22:09 +0100

    remove the explicit PhantomWalletAdapter from the wallets array in app/src/App.tsx

---
+++ tests/solanaform.ts
@@ -0,0 +1,17 @@
+// import * as anchor from "@coral-xyz/anchor";
+// import { Program } from "@coral-xyz/anchor";
+// import { Solanaform } from "../target/types/solanaform";
+
+// describe("solanaform", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   const program = anchor.workspace.solanaform as Program<Solanaform>;
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
